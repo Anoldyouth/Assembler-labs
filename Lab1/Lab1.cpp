6a65f83a8206d6da8d2a8c4fbd5f51f014cdbb31
Anoldyouth <83974142+Anoldyouth@users.noreply.github.com>
2022-02-16 16:09:06 +0300

    Update Lab1.cpp
    Добавил все тесты для заданий

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -61,13 +61,14 @@ template<class T>
 void print16(T t)
 {
     Bit16 s = {t};
-    cout << hex << *s.usht << endl;
-    cout << std::bitset<sizeof(*s.usht) * CHAR_BIT>(*s.usht) << endl;
-    cout << dec <<*s.usht << endl;
 
-    cout << hex << *s.sht << endl;
-    cout << std::bitset<sizeof(*s.sht) * CHAR_BIT>(*s.sht) << endl;
-    cout << dec << *s.sht << endl;
+    cout << "hex: " << hex << *s.usht << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.usht) * CHAR_BIT>(*s.usht) << endl;
+    cout << "dec: " << dec <<*s.usht << endl<< endl;
+
+    cout << "hex: " << hex << *s.sht << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.sht) * CHAR_BIT>(*s.sht) << endl;
+    cout << "dec: " << dec << *s.sht << endl<< endl;
 }
 
 template<class T>
@@ -76,24 +77,39 @@ void print32(T t)
     Bit32 s = {t};
 
     cout << "float" << endl;
-    cout << "the number itself: " << fixed << *s.flt << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.flt << endl;
+    cout << "hex: " << hex << *s.flt << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.flt) * CHAR_BIT>(*s.flt) << endl;
+    cout << "dec: " << dec <<*s.flt << endl;
+    cout << "fixed: " << fixed << *s.flt << endl;
+    cout << "exp: " << scientific << *s.flt << endl<< endl;
 
     cout << "int" << endl;
+    cout << "hex: " << hex << *s.ntgr << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.ntgr) * CHAR_BIT>(*s.ntgr) << endl;
+    cout << "dec: " << dec <<*s.ntgr << endl;
     cout << "the number itself: " << fixed << *s.ntgr << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.ntgr << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.ntgr << endl<< endl;
 
     cout << "unsigned int" << endl;
+    cout << "hex: " << hex << *s.untgr << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.untgr) * CHAR_BIT>(*s.untgr) << endl;
+    cout << "dec: " << dec <<*s.untgr << endl;
     cout << "the number itself: " << fixed << *s.untgr << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.untgr << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.untgr << endl<< endl;
 
     cout << "long" << endl;
+    cout << "hex: " << hex << *s.lng << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.lng) * CHAR_BIT>(*s.lng) << endl;
+    cout << "dec: " << dec <<*s.lng << endl;
     cout << "the number itself: " << fixed << *s.lng << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.lng << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.lng << endl<< endl;
 
     cout << "unsigned long" << endl;
+    cout << "hex: " << hex << *s.ulng << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.ulng) * CHAR_BIT>(*s.ulng) << endl;
+    cout << "dec: " << dec <<*s.ulng << endl;
     cout << "the number itself: " << fixed << *s.ulng << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.ulng << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.ulng << endl<< endl;
 }
 
 template<class T>
@@ -102,16 +118,25 @@ void print64(T t)
     Bit64 s = {t};
 
     cout << "double" << endl;
+    cout << "hex: " << hex << *s.dbl << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.dbl) * CHAR_BIT>(*s.dbl) << endl;
+    cout << "dec: " << dec <<*s.dbl <<  endl;
     cout << "the number itself: " << fixed << *s.dbl << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.dbl << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.dbl << endl<< endl;
 
     cout << "long long" << endl;
+    cout << "hex: " << hex << *s.lnglng << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.lnglng) * CHAR_BIT>(*s.lnglng) << endl;
+    cout << "dec: " << dec <<*s.lnglng <<  endl;
     cout << "the number itself: " << fixed << *s.lnglng << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.lnglng << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.lnglng << endl<< endl;
 
     cout << "unsigned long long" << endl;
+    cout << "hex: " << hex << *s.ulnglng << endl;
+    cout << "binary: " << std::bitset<sizeof(*s.ulnglng) * CHAR_BIT>(*s.ulnglng) << endl;
+    cout << "dec: " << dec <<*s.ulnglng << endl;
     cout << "the number itself: " << fixed << *s.ulnglng << endl;
-    cout << "the same number in the exponential form: " << scientific << *s.ulnglng << endl;
+    cout << "the same number in the exponential form: " << scientific << *s.ulnglng << endl<< endl;
 
 }
 
@@ -136,22 +161,174 @@ void printDump(const void* p, std::size_t N)
             cout << std::endl;
             line += lineLength;
         }
+        cout << dec;
     }
 
 int main()
 {
     sizeOfTypes();
-//    short number = -5;
-//    print16(&number);
 
-    int fltNum = 3926535;
-    print32(&fltNum);
-    cout << endl;
+    cout << endl << endl << endl << endl << "Testing print16()"  << endl << endl;
 
-    double dblNum = 2.7182818284;
-    print64(&dblNum);
+    unsigned short first = 0;
+    cout << "original number: " << first << endl;
+    print16(&first);
 
-    int x = 0x12345678;
-    printDump(&x, sizeof(x));
+    unsigned short second = (unsigned short)USHRT_MAX;
+    cout << "original number: " << second << endl;
+    print16(&second);
+
+    short third = SHRT_MIN;
+    cout << "original number: " << third << endl;
+    print16(&third);
+
+    short fourth = SHRT_MAX;
+    cout << "original number: " << fourth << endl;
+    print16(&fourth);
+
+    unsigned short fivth = 4;
+    cout << "original number: " << fivth << endl;
+    print16(&fivth);
+
+    short sixth = -7;
+    cout << "original number: " << sixth << endl;
+    print16(&sixth);
+
+    cout << endl << endl << endl << endl << "Testing print32()"  << endl << endl;
+
+    cout << "INTEGER" << endl << endl;
+
+    unsigned int ai = 0;
+    cout << "original number: " << ai << endl;
+    print32(&ai);
+
+    unsigned int bi = UINT_MAX;
+    cout << "original number: " << bi << endl;
+    print32(&bi);
+
+    int ci = INT_MIN;
+    cout << "original number: " << ci << endl;
+    print32(&ci);
+
+    int di = INT_MAX;
+    cout << "original number: " << di << endl;
+    print32(&di);
+
+    int ei = 0xF1F2F3F4;
+    cout << "original number: " << ei << endl;
+    print32(&ei);
+
+    int gi = 4;
+    cout << "original number: " << gi << endl;
+    print32(&gi);
+
+    int hi = -7;
+    cout << "original number: " << hi << endl;
+    print32(&hi);
+
+    cout << "LONG" << endl << endl;
+
+    unsigned long al = 0;
+    cout << "original number: " << al << endl;
+    print32(&al);
+
+    unsigned long bl = LONG_MAX;
+    cout << "original number: " << bl << endl;
+    print32(&bl);
+
+    long cl = LONG_MIN;
+    cout << "original number: " << cl << endl;
+    print32(&cl);
+
+    long dl = LONG_MAX;
+    cout << "original number: " << dl << endl;
+    print32(&dl);
+
+    long el = 0xF1F2F3F4;
+    cout << "original number: " << el << endl;
+    print32(&el);
+
+    long gl = 4;
+    cout << "original number: " << gl << endl;
+    print32(&gl);
+
+    long hl = -7;
+    cout << "original number: " << hl << endl;
+    print32(&hl);
+
+    cout << "FLOAT" << endl << endl;
+    float ef = 0xF1F2F3F4;
+    cout << "original number: " << ef << endl;
+    print32(&ef);
+
+    float gf = 4;
+    cout << "original number: " << gf << endl;
+    print32(&gf);
+
+    float hf = -7;
+    cout << "original number: " << hf << endl;
+    print32(&hf);
+
+    cout << endl << endl << endl << endl << "Testing print64()"  << endl << endl;
+
+    cout << "LONG LONG" << endl << endl;
+
+    unsigned long all = 0;
+    cout << "original number: " << all << endl;
+    print32(&all);
+
+    unsigned long long bll = LLONG_MAX;
+    cout << "original number: " << bll << endl;
+    print32(&bll);
+
+    long long cll = LLONG_MIN;
+    cout << "original number: " << cll << endl;
+    print32(&cll);
+
+    long long dll = LLONG_MAX;
+    cout << "original number: " << dll << endl;
+    print32(&dll);
+
+    long long ell = 0xF1F2F3F4;
+    cout << "original number: " << ell << endl;
+    print32(&ell);
+
+    long long gll = 4;
+    cout << "original number: " << gll << endl;
+    print32(&gll);
+
+    long long hll = -7;
+    cout << "original number: " << hll << endl;
+    print32(&hll);
+
+    cout << "DOUBLE" << endl << endl;
+    double ed = 0xF1F2F3F4;
+    cout << "original number: " << ed << endl;
+    print32(&ed);
+
+    double gd = 4;
+    cout << "original number: " << gd << endl;
+    print32(&gd);
+
+    double hd = -7;
+    cout << "original number: " << hd << endl;
+    print32(&hd);
+
+    cout << endl << endl << endl << endl << "Testing ptintDump()"  << endl << endl;
+    int a = 0xF1F2F3F4;
+    cout << "original number: " << a << endl;
+    printDump(&a, sizeof(a));
+
+    int b[3];
+    b[0] = 0xF1F2F3F4;
+    b[1] = 4;
+    b[2] = -7;
+    cout << "original number: " << b[0] << " " << b[1] << " " << b[2] << endl;
+    printDump(&b, sizeof(b));
+
+    double c = 4;
+    cout << "original number: " << c << endl;
+    printDump(&c, sizeof(c));
+
     return 0;
 }
